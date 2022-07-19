716b2ba852b6d1d8250ecc0d632de9bd16295f47
Rohan66 <78190074+Rohan66@users.noreply.github.com>
2022-07-18 18:46:16 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload()
+function setup(){
+    classifyCanvas
+}
+function draw(){
+    strokeWeight: 34px;
+    stoke-color: purple;
+}
+if
+function line(pmouseX, pmouseY, mouseX, mouseY)
+classifyCanvas{
+    classifier.classify()
+}
+
